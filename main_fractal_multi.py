87c82e33afaf5f327a12a86d6fe490439ec78e01
ecofractal <ourfractal@riseup.net>
2019-04-15 23:54:50 +0200

    change instrument types at main_fractal_multi.py

--- main_fractal_multi.py
+++ main_fractal_multi.py
@@ -17,15 +17,15 @@ test = fractal.normalize_climate_multi(df)
 
 # Let's add some instruments to each track!
 instruments_to_add = [
-    'music box', 'baritone sax', 'low bongo'
+    'violin', 'hi wood block', 'melodic tom'
 ]
 
 multitrack_data_with_instruments = []
 for index, track in enumerate(test):
     multitrack_data_with_instruments.append([instruments_to_add[index]] + track)
 
-# While we're at it, let's add a drum track with a solid beat
-max_number_of_beats = multitrack_data_with_instruments[0][-1][0]
+# test a drum track with a solid beat
+max_number_of_beats = multitrack_data_with_instruments[1][-2][1]
 
 bass_drum = []
 for beat in range(0, int(max_number_of_beats + 1)):
@@ -34,4 +34,4 @@ for beat in range(0, int(max_number_of_beats + 1)):
 beat_track = ['bass drum 1'] + bass_drum
 multitrack_data_with_instruments.append(beat_track)
 
-play_midi_from_data(multitrack_data_with_instruments, track_type='multiple', key='c_sharp_major')
+play_midi_from_data(multitrack_data_with_instruments, track_type='multiple', key='f_major')
